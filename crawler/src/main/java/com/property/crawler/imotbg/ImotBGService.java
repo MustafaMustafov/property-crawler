508bc8cb76d102c1edf8bf606872f6f2c7a7adf1
MustafaMustafov <musitoahmed@gmail.com>
2024-07-23 11:13:47 +0300

    feature/add-searching-form - update request to imotBG

--- crawler/src/main/java/com/property/crawler/imotbg/ImotBGService.java
+++ crawler/src/main/java/com/property/crawler/imotbg/ImotBGService.java
@@ -46,7 +46,13 @@ public class ImotBGService {
             HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
 
-            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+//            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+            con.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            con.setRequestProperty("Accept-Language", "en-US,en;q=0.9");
+            con.setRequestProperty("Connection", "keep-alive");
+
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             StringBuilder response = new StringBuilder();
