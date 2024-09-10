302afcf78e3f20b09174ecc90fe9382d1ba8bd93
civelin <tsvetelinaivelin@gmail.com>
2024-09-10 12:06:42 +0300

    fix search_query_object type

--- crawler/src/main/java/com/property/crawler/entity/SearchRecord.java
+++ crawler/src/main/java/com/property/crawler/entity/SearchRecord.java
@@ -1,12 +1,7 @@
 package com.property.crawler.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +18,7 @@ public class SearchRecord {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "search_object", nullable = false)
+    @Column(name = "search_object", nullable = false, columnDefinition = "TEXT")
     private String searchObject;
 
     @Column(name = "generated_file", columnDefinition = "BYTEA")
